e9aad9a7146633f2bedf37d24ce785bb81038800
st114908 <st114908@stud.uni-stuttgart.de>
2024-05-07 12:56:31 +0200

    Error reason found (online Config.hpp file has been changed) and cleanup.

--- Comparison/deployable-files/fastCarDriverECU/CI_FRONTDISTANCESENSORFDISTANCESENSORdistancePortaccessCommand.c
+++
@@ -1,7 +0,0 @@
-#include <SimpleHardwareController_Connector.h>
-#include "CI_FRONTDISTANCESENSORFDISTANCESENSORdistancePortaccessCommand.h"
-void CI_FRONTDISTANCESENSORFDISTANCESENSORdistancePortaccessCommand(int32_T* distance){
-	// Start of user code API
-	*distance = SimpleHardwareController_DistanceSensor_GetDistanceToClosestMm(0);
-	// End of user code
-}
