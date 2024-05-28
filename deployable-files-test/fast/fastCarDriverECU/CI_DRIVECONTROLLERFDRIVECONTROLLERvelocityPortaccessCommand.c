ffa0a0864da007d937c50db988b3b29980141e76
st114908 <st114908@stud.uni-stuttgart.de>
2024-05-28 16:09:12 +0200

    Error search: First upload for comparison.

---
+++ deployable-files-test/fast/fastCarDriverECU/CI_DRIVECONTROLLERFDRIVECONTROLLERvelocityPortaccessCommand.c
@@ -0,0 +1,7 @@
+#include <SimpleHardwareController_Connector.h>
+#include "CI_DRIVECONTROLLERFDRIVECONTROLLERvelocityPortaccessCommand.h"
+void CI_DRIVECONTROLLERFDRIVECONTROLLERvelocityPortaccessCommand(int16_T* velocity){
+	// Start of user code API
+	*velocity = SimpleHardwareController_DriveController_GetSpeed();
+	// End of user code
+}
---
+++ deployable-files-test/fast/fastCarDriverECU/CI_DRIVECONTROLLERFDRIVECONTROLLERvelocityPortaccessCommand.h
@@ -0,0 +1,15 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+// Start of user code FORINCLUDES
+
+// End of user code
+#include "standardTypes.h"
+#include "customTypes.h"
+void CI_DRIVECONTROLLERFDRIVECONTROLLERvelocityPortaccessCommand(int16_T* velocity);
+// Start of user code API
+
+// End of user code
+#ifdef __cplusplus
+}
+#endif
